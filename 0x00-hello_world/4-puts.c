458c2c35a0f7bafcdf3fe4bb6473d43ed44c648f
AndileFaku <andilefaku@gmail.com>
2023-09-09 08:14:17 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Printing with puts funtion instead of printf
+ * Return: 0 (Success)
+ */
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
