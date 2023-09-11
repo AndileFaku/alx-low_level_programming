b671654dfe7567722c2c2bde40252e7eb6caf05a
AndileFaku <andilefaku@gmail.com>
2023-09-11 14:38:38 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point for single digit numbers of base 10
+ * Return: 0 (success)
+ */
+
+int main()
+{
+	int a;
+
+	for(a = 0; a < 10; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}
