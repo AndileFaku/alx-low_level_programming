afc541c94a665d3a7ba2f86169d2cfe3f19ac33b
AndileFaku <andilefaku@gmail.com>
2023-09-11 16:00:42 -0700

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point for program printing hexadecimals
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int x;
+
+	char c;
+
+	x = 0;
+	c = 'a';
+	while
+		(x < 10)
+		{
+			putchar(x + '0');
+			x++;
+		}
+	while
+		(c = 'a'; c <= 'f')
+		{
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
