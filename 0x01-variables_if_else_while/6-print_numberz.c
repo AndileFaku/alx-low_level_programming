26483492d6c394c368575b61d0eee9a82c945c22
AndileFaku <andilefaku@gmail.com>
2023-09-11 14:54:13 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	while
+		(a < 10)
+		{
+			putchar(a);
+			a++;
+		}
+	putchar('\n');
+	return (0);
+}
